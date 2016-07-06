a00730cc1f03eebcb74192016caff473e39575dd
ekasteel <evert.kasteel@live.com>
2016-07-06 11:11:07 +0000

    Selenium IDE C# export example

---
+++ test/selenium_ide001.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class SeleniumIde001
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.nl/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheSeleniumIde001Test()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/?gws_rd=ssl");
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("google maps");
+            driver.FindElement(By.Name("btnG")).Click();
+            for (int second = 0;; second++) {
+                if (second >= 60) Assert.Fail("timeout");
+                try
+                {
+                    if (IsElementPresent(By.LinkText("Google Maps"))) break;
+                }
+                catch (Exception)
+                {}
+                Thread.Sleep(1000);
+            }
+            driver.FindElement(By.LinkText("Google Maps")).Click();
+            try
+            {
+                Assert.AreEqual("Google Maps", driver.Title);
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
